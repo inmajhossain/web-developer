263767ddc9ed8270ab4074f4a50a74e74cf09204
inmajhossain <inmaj670@gmail.com>
2024-12-09 21:39:38 +0600

    ..

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+//   daisyui: {
+//     themes: ["light", "dark", "cupcake", "night", "sunset"],
+//   },
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [require("daisyui")],
+// };
+
+//new new
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   daisyui: {
     themes: ["light", "dark", "cupcake", "night", "sunset"],
